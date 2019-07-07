39674ba0f1cf0a7f0edf7a5112694b72fff2985a
Taylor Schumacher <tschumacher255@gmail.com>
2019-07-07 14:59:52 -0500

    init commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Daily_Coding_Problem_32
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
